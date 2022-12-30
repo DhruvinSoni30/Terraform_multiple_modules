956a883c6bc805c1a7aead463353d9cd7270b7d0
Dhruvin Soni <37222087+DhruvinSoni30@users.noreply.github.com>
2022-12-30 20:01:02 +0530

    Add files via upload

--- prod/main.tf
+++ prod/main.tf
@@ -4,10 +4,8 @@ module "vpc" {
   cidr_block        = var.cidr_block
   vpc_id            = module.vpc.vpc_id
   cidr_block_subnet = var.cidr_block_subnet
-  name_vpc          = var.name_vpc
-  env_vpc           = var.env_vpc
-  name_subnet       = var.name_subnet
-  env_subnet        = var.env_subnet
+  name              = var.name
+  tenancy           = var.tenancy
 }
 
 module "ec2" {
@@ -16,11 +14,7 @@ module "ec2" {
 
   ami_id            = var.ami_id
   instance_type     = var.instance_type
-  availability_zone = var.availability_zone
   region            = var.region
   subnet_id         = module.vpc.subnet_id
-  name_ec2          = var.name_ec2
-  env_ec2           = var.env_ec2
-
-
+  name              = var.name
 }
