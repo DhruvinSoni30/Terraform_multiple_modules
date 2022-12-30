997f0c8c29b4ed2c712309e9ac4d3767b8e3fc2a
Dhruvin Soni <37222087+DhruvinSoni30@users.noreply.github.com>
2022-12-30 20:01:45 +0530

    Add files via upload

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,13 +1,14 @@
 resource "aws_instance" "demo_instance" {
 
-  ami               = var.ami_id
-  instance_type     = var.instance_type
-  availability_zone = var.availability_zone
-  subnet_id         = var.subnet_id
+ami               = var.ami_id
+instance_type     = var.instance_type
+availability_zone = data.aws_availability_zones.az.names[0]
+subnet_id         = var.subnet_id
 
-  tags = {
-    "Name" = var.name_ec2
-    "Env"  = var.env_ec2
+ tags = {
+    "Name" = "${var.name}-instance"
   }
-
 }
+
+# AZ
+data "aws_availability_zones" "az" {}
--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -7,14 +7,8 @@ variable "ami_id" {}
 # Instance Type
 variable "instance_type" {}
 
-# AZ
-variable "availability_zone" {}
-
 # Subnet ID
 variable "subnet_id" {}
 
-# Tags for EC2 Instance
-variable "name_ec2" {}
-
-variable "env_ec2" {}
-
+# Tags 
+variable "name" {}
